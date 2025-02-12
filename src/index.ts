9e408043c081b46144219963f0413898078b3fdb
Oscar gracia <113419592+OscarGrC@users.noreply.github.com>
2025-02-12 14:10:09 +0100

    middleware create  commit

--- src/index.ts
+++ src/index.ts
@@ -79,7 +79,7 @@ app.get('/live', (req: Request, res: Response) => {
     res.send(`${new Date().toISOString()}`);
 });
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 
 app.listen(port, () => {
